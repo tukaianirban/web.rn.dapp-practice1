24ecb72d3876afa6808983b73421f8c274c54739
tukaianirban <mukherjee.anirban86@gmail.com>
2022-12-27 18:14:42 +0100

    added in most form components in Welcome and Loader component

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,17 +4,35 @@ import { BsInfoCircle } from 'react-icons/bs';
 
 import { Loader } from './';
 
-const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
+const commonStyles = 'min-h-[70px] sm:px-0 px-2 flex justify-center items-center border-[0.5px] \
+border-gray-400 text-sm font-light text-white';
 
+const Input = ({placeholder, name, type, value, handleChange}) => {
+    return (
+        <input 
+            placeholder={placeholder}
+            type={type}
+            step="0.0001"
+            value={value}
+            onChange={() => handleChange(e, name)}
+            className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
+            />
+    );
+}
 const Welcome = () => {
 
     const connectWallet = () => {
 
     }
 
+    // called when user presses the Submit button in the form
+    const handleSubmit = () => {
+
+    }
+
     return (
         <div className='flex w-full justify-center items-center'>
-            <div className='flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
+            <div className='flex md:flex-row flex-col items-start justify-between md:p-10 py-12 px-1'>
                 <div className='flex flex-1 justify-start flex-col md:mr-10'>
                     <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1'>
                         Send Crypto <br/> across the world
@@ -26,7 +44,7 @@ const Welcome = () => {
                         <p className='text-white text-base font-semibold'>Connect Wallet</p>
                     </button>
 
-                    <div className='grid sm:grid-col-3 grid-cols-2 w-full mt-10'>
+                    <div className='grid grid-cols-3 w-full mt-10'>
                         <div className={`rounded-tl-2xl ${commonStyles}`}>
                             Reliability
                         </div>
@@ -36,8 +54,66 @@ const Welcome = () => {
                         <div className={`rounded-tr-2xl ${commonStyles}`}>
                             Ethereum
                         </div>
+                        <div className={`rounded-bl-2xl ${commonStyles}`}>
+                            Web 3.0
+                        </div>
+                        <div className={commonStyles}>
+                            Low fees
+                        </div>
+                        <div className={`rounded-br-2xl ${commonStyles}`}>
+                            Blockchain
+                        </div>
                     </div>
                 </div>
+
+                {/* the right side of our desktop Welcome view; the entry form and  */}
+                <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10'>
+                    {/* the eth-card is a special css definition in the index.css */}
+                    <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism'>
+                        <div className='flex justify-between flex-col w-full h-full'>
+                            {/* the ethereum icon */}
+                            <div className='flex justify-between items-start'>
+                                <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
+                                    <SiEthereum fontSize={21} color='#fff' />
+                                </div>
+                                {/* the info icon inside the card */}
+                                <BsInfoCircle fontSize={17} color='#fff' />
+                            </div>
+                            <div>
+                                {/* display teh currently connected wallet address */}
+                                <p className='text-white font-light text-sm'>
+                                    0xkjdsgd;gdkhgvkd .... fagsgdavgdh
+                                </p>
+                                <p className='text-white font-semibold text-lg mt-1'>
+                                    Ethereum
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
+                        <Input placeholder="Address To" name="addressTo" type='text' handleChange={ () => {}} />
+                        <Input placeholder="Amount (ETH)" name="amount" type='number' handleChange={ () => {}} />
+                        <Input placeholder="Keyword (Gif)" name="keyword" type='text' handleChange={ () => {}} />
+                        <Input placeholder="Enter message" name="message" type='text' handleChange={ () => {}} />
+                    
+                        {/* the grayish line below the form fields */}
+                        <div className='h-[1px] w-full bg-gray-400 my-2' />
+
+                        {true 
+                            ? (<Loader />)
+                            : (
+                                <button 
+                                    type='button'
+                                    onClick={handleSubmit}
+                                    className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer'>
+                                        Send Now
+                                </button>
+                            )
+                        }
+                    </div>
+                </div>
+
             </div>
         </div>
     );
