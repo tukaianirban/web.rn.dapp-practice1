24af6833b6a1bdb2c479bfd8e3394f277930dff3
tukaianirban <mukherjee.anirban86@gmail.com>
2022-12-29 06:17:50 +0100

    impl: Services section

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,8 +1,62 @@
+import { BsShieldFillCheck } from 'react-icons/bs';
+import { BiSearchAlt } from 'react-icons/bi';
+import { RiHeart2Fill } from 'react-icons/ri';
+
+const ServiceCard = ({ color, title, icon, subtitle }) => {
+
+    return (
+        <div className='flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl'>
+            <div className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}>
+                {icon}
+            </div>
+            <div className='m-5 flex flex-col flex-1'>
+                <h1 className='mt-2 text-white text-lg'>{title}</h1>
+                <p className='mt-2 text-white text-sm md:w-9/12'>{subtitle}</p>
+            </div>
+        </div>
+    );
+}
+
 const Services = () => {
     return (
-        <h1>
-            Services
-        </h1>
+        // flex-col for any device and flex-row for md and larger devices
+        <div className='flex flex-col md:flex-row w-full justify-center items-center gradient-bg-services'>
+
+            {/* the heading of services section */}
+            <div className='flex mf:flex-row flex-col items-center jusitfy-between md:p-20 py-12 px-4'>
+                <div className='flex-1 flex flex-col justify-start items-start'>
+                    <h1 className='text-white text-3xl sm:text-5xl py-2 text-gradient'>Services that we 
+                        <br />
+                        continue to improve
+                    </h1>
+                </div>
+            </div>
+
+            {/* the services cards */}
+            <div className='flex-1 flex flex-col justify-start items-center'>
+                <ServiceCard
+                    color='bg-[#2952e3]'
+                    title='Security guaranteed'
+                    icon={<BsShieldFillCheck fontSize={21} className='text-white' />}
+                    subtitle='Security is guaranteed. We always maintain privacy quality of our products'
+                />
+
+                <ServiceCard
+                    color='bg-[#8984F8]'
+                    title='Best exchange rates'
+                    icon={<BiSearchAlt fontSize={21} className='text-white' />}
+                    subtitle='Security is guaranteed. We always maintain privacy quality of our products'
+                />
+
+                <ServiceCard
+                    color='bg-[#F84550]'
+                    title='Fastest transactions'
+                    icon={<RiHeart2Fill fontSize={21} className='text-white' />}
+                    subtitle='Security is guaranteed. We always maintain privacy quality of our products'
+                />
+            </div>
+
+        </div>
     );
 }
 
